88445d5492194efe11af61072bf96933f04253b5
sabihajyoti22 <sabihajyoti@gmail.com>
2024-11-15 19:05:27 +0600

    feat: with cache

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,12 +1,12 @@
-// const staticCacheName = "site-static-v1"
-// const dynamicCacheName = "site-dynamic-v1"
-// const offlineURL = '/src/views/Offline.vue'
-// const assets = [
-//   '/src/style.css',
-//   '/src/assets/fonts/LilitaOne-Regular.ttf',
-//   '/images/icon-144x144.png',
-//   '/src/views/Offline.vue'
-// ]
+const staticCacheName = "site-static-v1"
+const dynamicCacheName = "site-dynamic-v1"
+const offlineURL = '/src/views/Offline.vue'
+const assets = [
+  '/src/style.css',
+  '/src/assets/fonts/LilitaOne-Regular.ttf',
+  '/images/icon-144x144.png',
+  '/src/views/Offline.vue'
+]
 
 let db = null
 let objectStore = null
@@ -128,44 +128,44 @@ channel2.onmessage = (event) => {
 
 self.addEventListener('install', evt => {
   console.log('Service worker installed')
-  // evt.waitUntil(
-  //   caches.open(staticCacheName).then(cache => {
-  //     cache.addAll(assets)
-  //   }).catch(err => {
-  //     console.log(err)
-  //   })
-  // )
+  evt.waitUntil(
+    caches.open(staticCacheName).then(cache => {
+      cache.addAll(assets)
+    }).catch(err => {
+      console.log(err)
+    })
+  )
 })
 
 self.addEventListener('activate', evt => {
   console.log("Service worker activated")
-  // evt.waitUntil(
-  //   caches.keys().then(keys => {
-  //     return Promise.all(keys
-  //       .filter(key => key !== staticCacheName)
-  //       .map(key => caches.delete(key))
-  //     )
-  //   }).catch(err => {
-  //     console.log(err)
-  //   })
-  // )
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== staticCacheName)
+        .map(key => caches.delete(key))
+      )
+    }).catch(err => {
+      console.log(err)
+    })
+  )
 })
 
-// self.addEventListener("fetch", (evt) => {
-//   evt.respondWith(
-//     caches.match(evt.request).then(cacheRes => {
-//       return cacheRes || fetch(evt.request)
-//         .then(fetchRes => {
-//           return caches.open(dynamicCacheName).then(cache => {
-//             cache.put(evt.request.url, fetchRes.clone())
-//             return fetchRes
-//           })
-//         })
-//         .catch(() => {
-//           if (evt.request.url.indexof('.vue') >= 0) {
-//             return caches.match(offlineURL)
-//           }
-//         })
-//     })
-//   )
-// })
+self.addEventListener("fetch", (evt) => {
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request)
+        .then(fetchRes => {
+          return caches.open(dynamicCacheName).then(cache => {
+            cache.put(evt.request.url, fetchRes.clone())
+            return fetchRes
+          })
+        })
+        .catch(() => {
+          if (evt.request.url.indexof('.vue') >= 0) {
+            return caches.match(offlineURL)
+          }
+        })
+    })
+  )
+})
